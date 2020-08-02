aa44291801cb31adbabd51ab6328d5db0921d466
alabobriggs <alabobriggs@gmail.com>
2020-08-02 08:34:13 +0100

    get products data from provider

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,29 @@
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_app/providers/products_provider.dart';
+import '../widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, idx) => ProductItem(
+        id: products[idx].id,
+        title: products[idx].title,
+        imageUrl: products[idx].imageUrl,
+      ),
+    );
+  }
+}
