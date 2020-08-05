7a08ed8b96ff1207c4adcc69ef9235d334ec62fb
alabobriggs <alabobriggs@gmail.com>
2020-08-05 10:06:55 +0100

    add orders provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './providers/orders.dart';
 import './providers/cart.dart';
 import './screens/cart_screen.dart';
 import './providers/products.dart';
@@ -18,6 +19,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (ctx) => CartProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => OrdersProvider(),
         ),
       ],
       child: MaterialApp(
---
+++ lib/providers/orders.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import '../models/cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({
+    @required this.id,
+    @required this.amount,
+    @required this.products,
+    @required this.dateTime,
+  });
+}
+
+class OrdersProvider with ChangeNotifier {
+  List<OrderItem> _items = [];
+
+  List<OrderItem> get orders {
+    return [...orders];
+  }
+
+  void addOrder(List<CartItem> cartProducts, double total) {
+    _items.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+
+    notifyListeners();
+  }
+
+  void clearOrders() {
+    _items = [];
+    notifyListeners();
+  }
+}
