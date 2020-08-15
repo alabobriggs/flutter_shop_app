a41027a7d655abf666fd210d46d73e512136969c
alabobriggs <alabobriggs@gmail.com>
2020-08-15 07:16:30 +0100

    add method to update favourite on server directly

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/http_exception.dart';
 
 class ProductModelProvider with ChangeNotifier {
   final String id;
@@ -17,8 +20,28 @@ class ProductModelProvider with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus() {
-    isFavourite = !isFavourite;
+  void _setFavValue(bool newValue) {
+    isFavourite = newValue;
     notifyListeners();
+  }
+
+  Future<void> toggleFavouriteStatus() async {
+    final oldStatus = isFavourite;
+    _setFavValue(!isFavourite);
+
+    try {
+      final http.Response response = await http.patch(
+        'https://shop-app-2170f.firebaseio.com/products/$id.json',
+        body: json.encode({
+          'isFavourite': !isFavourite,
+        }),
+      );
+
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (err) {
+      _setFavValue(oldStatus);
+    }
   }
 }
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop_app/models/http_exception.dart';
+import '../models/http_exception.dart';
 import './product.dart';
 
 class ProductsProvider with ChangeNotifier {
